43e78b65e7f2c398c1c47aee79f3dfe9abbfa087
Nabil <22-49909-3@student.aiub.edu>
2025-09-03 22:07:44 +0600

    login page

---
+++ view/html/program.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+
+    </head>
+    <body>
+        <h2> Login Page </h2>
+        <form>
+        <label for="fname">First Name:</label>
+        <input type="text" id="fname" name="fname"><br><br>
+        <label for="fid">ID:</label>
+        <input for="fid" id="fid" name="fid"><br><br>
+        <input type="submit" Value="Submit">
+        </form>
+
+    </body>
+</html>
